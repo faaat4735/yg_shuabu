b53944c4272716b390cf159b110cbb5810e2897f
faaat <zjf580@163.com>
2020-11-11 14:57:54 +0800

    删除

--- config/config.private.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('DB_HOST', 'rm-bp170dg65324p0u40zo.mysql.rds.aliyuncs.com');
-define('DB_USERNAME', 'test_write');
-define('DB_PASSWORD', 'Yonggui1007');
-define('DB_DATABASE', 'yg_shuabu');
