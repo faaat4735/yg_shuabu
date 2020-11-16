9b61e42799ebd2136430ee4b4c787eecd5dbcdfe
faaat <zjf580@163.com>
2020-11-16 10:03:03 +0800

    修复步数奖励接口

--- app/core/Task/Box.php
+++ app/core/Task/Box.php
@@ -13,7 +13,7 @@ class Box extends Task
         if ($activityInfo['activity_max'] > $activityReceiveInfo['count']) {
             $sql = 'SELECT award_min, award_max FROM t_award_config WHERE config_type = ? AND counter <= ? ORDER BY counter DESC LIMIT 1';
             $awardRange = $this->db->getRow($sql, 'box', $activityReceiveInfo['count'] + 1);
-            $return = array('count' => $activityReceiveInfo['count'] + 1, 'num' => rand($awardRange['award_min'], $awardRange['award_max']),'type' => 'box', 'receiveTime' => (($activityReceiveInfo['maxTime'] ? strtotime($activityReceiveInfo['maxTime']) : time()) + $activityInfo['activity_duration'] * 60) * 1000);
+            $return = array('count' => $activityReceiveInfo['count'] + 1, 'num' => rand($awardRange['award_min'], $awardRange['award_max']),'type' => 'box', 'receiveTime' => (($activityReceiveInfo['maxTime'] ? strtotime($activityReceiveInfo['maxTime']) + $activityInfo['activity_duration'] * 60 : time())) * 1000);
         }
         $return['maxReceive'] = $activityInfo['activity_max'];
         $return['currentReceive'] = $activityReceiveInfo['count'];
