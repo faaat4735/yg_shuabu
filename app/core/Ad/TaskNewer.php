74680d00915a71d28b6f185f4f9ef63d812658fd
faaat <zjf580@163.com>
2020-12-22 23:14:21 +0800

    修复任务完成情况

--- app/core/Ad/TaskNewer.php
+++ app/core/Ad/TaskNewer.php
@@ -9,6 +9,10 @@ class TaskNewer extends Ad
     protected function _getInfo() {
         $sql = 'SELECT award_min FROM t_award_config WHERE config_type = "wechat"';
         $wechatAward = $this->db->getOne($sql);
-        return array(array('name' => '绑定微信', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'wechat', 'award' => $wechatAward, 'isComplete' => 1), array('name' => '填写邀请码', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'invitedCode', 'award' => 2000, 'isComplete' => 0));
+        $sql = 'SELECT wechat_unionid FROM t_user WHERE user_id = ?';
+        $isBindWechat = $this->db->getOne($sql) ? 1 : 0;
+        $sql = 'SELECT id FROM t_user_invited WHERE invited_id = ?';
+        $isBindInvited = $this->db->getOne($sql) ? 1 : 0;
+        return array(array('name' => '绑定微信', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'wechat', 'award' => $wechatAward, 'isComplete' => $isBindWechat), array('name' => '填写邀请码', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'invitedCode', 'award' => 500, 'isComplete' => $isBindInvited));
     }
 }
