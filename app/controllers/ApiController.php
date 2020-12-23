964271a54d35b6c488295ec7f8d12c5d7f2c0724
faaat <zjf580@163.com>
2020-12-23 14:36:12 +0800

    添加监测链接

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -2,10 +2,13 @@
 
 namespace Controller;
 
-use Core\Controller;
-
-class ApiController extends Controller
+class ApiController
 {
+
+    public function init() {
+
+    }
+
     public function monitorAction () {
         // todo 添加保留监测链接数据
         return array();
