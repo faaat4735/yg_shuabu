515afa427a2459d110912a0dc334a91cbd04c481
faaat <zjf580@163.com>
2020-12-28 16:11:56 +0800

    修复任务页

--- app/core/Ad/TaskNewer.php
+++ app/core/Ad/TaskNewer.php
@@ -10,9 +10,9 @@ class TaskNewer extends Ad
         $sql = 'SELECT award_min FROM t_award_config WHERE config_type = "wechat"';
         $wechatAward = $this->db->getOne($sql);
         $sql = 'SELECT wechat_unionid FROM t_user WHERE user_id = ?';
-        $isBindWechat = $this->db->getOne($sql) ? 1 : 0;
+        $isBindWechat = $this->db->getOne($sql, $this->userId) ? 1 : 0;
         $sql = 'SELECT id FROM t_user_invited WHERE invited_id = ?';
-        $isBindInvited = $this->db->getOne($sql) ? 1 : 0;
+        $isBindInvited = $this->db->getOne($sql, $this->userId) ? 1 : 0;
         return array(array('name' => '绑定微信', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'wechat', 'award' => $wechatAward, 'isComplete' => $isBindWechat), array('name' => '填写邀请码', 'desc' => '让每一步更有价值', 'type' => 'task', 'url' => 'invitedCode', 'award' => 500, 'isComplete' => $isBindInvited));
     }
 }
