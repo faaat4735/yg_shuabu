1c8c088e840c5a83d3fd15a3984895f8d9a5edb7
faaat <zjf580@163.com>
2020-12-29 19:27:01 +0800

    保留监测数据

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -14,6 +14,9 @@ class ApiController extends Controller
         if ($_GET) {
             $sql = 'INSERT INTO t_ocean_monitor SET imei_md5 = ?, oaid = ?, androidid_md5 = ?, mac_md5 = ?, ad_id = ?, params = ?';
             $this->db->exec($sql, $_GET['imei'] ?? '', $_GET['oaid'] ?? '', $_GET['androidid'] ?? '', $_GET['mac'] ?? '', $_GET['adid'] ?? '', json_encode($_GET));
+            if (isset($_GET['callback'])) {
+                file_get_contents($_GET['callback']);
+            }
         }
         $return = array('code' => '200', 'msg' => '保存成功');
         echo json_encode($return);
