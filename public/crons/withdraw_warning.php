dddf921f72ae4ec9997b949ad04fcf262bc3677d
faaat <zjf580@163.com>
2021-01-06 14:55:12 +0800

    提现处理提醒邮件

--- public/crons/withdraw_warning.php
+++ public/crons/withdraw_warning.php
@@ -2,24 +2,16 @@
 
 require_once __DIR__ . '/../init.inc.php';
 
-//$endHour = date('H');
-//
-//if ($endHour > 6) {
-//    $last = 1;
-//} elseif ($endHour == 6) {
-//    $last = 6;
-//} elseif ($endHour == 0) {
-//    $last = 1;
-//} else {
-//    exit;
-//}
+$endHour = date('H');
+
+if ($endHour > 0 && $endHour < 8) {
+    exit;
+}
 
 //$startHour = date('H:00', strtotime('-' . $last . ' hours'));
 //$endHour = date('H:00');
 //$startTime = date('Y-m-d H:00:00', strtotime('-' . $last . ' hour'));
 //$endTime = date('Y-m-d H:00:00');
-$startTime = date('Y-m-d 00:00:00', strtotime('-1 day'));
-$endTime = date('Y-m-d 23:59:59', strtotime('-1 day'));
 
 $html = '<html lang="en">
 <head>
@@ -35,14 +27,14 @@ $str = '<table>
 </table>';
 $db = \Core\Db::getDbInstance();
 
-$sql = 'SELECT COUNT(withdraw_id) count, SUM(withdraw_amount) sum FROM t_withdraw WHERE create_time >= ? AND create_time < ? AND withdraw_status = "pending"';
-$total = $db->getRow($sql, $startTime, $endTime);
+$sql = 'SELECT COUNT(withdraw_id) count, SUM(withdraw_amount) sum FROM t_withdraw WHERE withdraw_status = "pending"';
+$total = $db->getRow($sql);
 
 $html .= sprintf($str, $total['count'] ?: 0, $total['sum'] ?: 0);
 
 $html .= '</body>
 </html>';
 $mail = new \Core\Mail();
-$mail->send(array('zjf580@163.com'), (ENV_PRODUCTION ? '' : '测试-') . date('Y-m-d', strtotime('-1 day')) . '定时提现邮件', $html);
+$mail->send(array('zjf580@163.com'), (ENV_PRODUCTION ? '' : '测试-') . '未处理提现定时提现邮件', $html);
 echo 'done' . PHP_EOL;
 
