f96bc8a86273b49d0eaa6cee9eb5505f2a2e4b6d
faaat <zjf580@163.com>
2021-03-05 15:31:30 +0800

    添加运动赚入口

--- app/core/Ad/StartMid.php
+++ app/core/Ad/StartMid.php
@@ -8,6 +8,6 @@ class StartMid extends Ad
 {
     protected function _getInfo() {
 
-        return array(array('img' => OSS_HOST . 'ad/startMid1.png', 'type' => 'interior', 'url' => 'task'), array('img' => OSS_HOST . 'ad/startMid2.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/startMid3.png', 'type' => 'interior', 'url' => 'task'));
+        return array(array('img' => OSS_HOST . 'ad/startMid1.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/startMid2.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/startMid3.png', 'type' => 'interior', 'url' => 'task'));
     }
 }
--- app/core/Ad/TaskDaily.php
+++ app/core/Ad/TaskDaily.php
@@ -7,6 +7,6 @@ use Core\Ad;
 class TaskDaily extends Ad
 {
     protected function _getInfo() {
-        return array(array('name' => '步数累计任务', 'desc' => '让每一步更有价值', 'type' => 'interior', 'url' => 'walkStage', 'award' => 1000), array('name' => '每日打卡', 'desc' => '一天8杯水，健康靠积累', 'type' => 'interior', 'url' => 'clockIn', 'award' => 1500), array('name' => '邀请好友', 'desc' => '和好友一起走路赚钱', 'type' => 'interior', 'url' => 'invited', 'award' => 5000));
+        return array(array('name' => '步数累计任务', 'desc' => '让每一步更有价值', 'type' => 'interior', 'url' => 'walkStage', 'award' => 1000), array('name' => '每日打卡', 'desc' => '一天8杯水，健康靠积累', 'type' => 'interior', 'url' => 'clockIn', 'award' => 1500), array('name' => '运动一下', 'desc' => '出门运动运动，强身健体', 'type' => 'interior', 'url' => 'sport', 'award' => 1800), array('name' => '邀请好友', 'desc' => '和好友一起走路赚钱', 'type' => 'interior', 'url' => 'invited', 'award' => 5000));
     }
 }
