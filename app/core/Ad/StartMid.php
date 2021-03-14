0bdaa6a7fb20e89cf9f69575d9098ca72e38520a
faaat <zjf580@163.com>
2021-03-14 15:04:54 +0800

    第三方变现入口

--- app/core/Ad/StartMid.php
+++ app/core/Ad/StartMid.php
@@ -8,6 +8,8 @@ class StartMid extends Ad
 {
     protected function _getInfo() {
 
-        return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
+
+        return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'sdk', 'url' => 'op_novel'));
+//        return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
     }
 }
