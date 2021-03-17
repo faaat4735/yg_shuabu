f3523fa593813315b27795291894c0bf743c5158
faaat <zjf580@163.com>
2021-03-17 11:11:09 +0800

    更换首页图标

--- app/core/Ad/StartMid.php
+++ app/core/Ad/StartMid.php
@@ -7,9 +7,10 @@ use Core\Ad;
 class StartMid extends Ad
 {
     protected function _getInfo() {
-
-
-        return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'sdk', 'url' => 'op_novel'));
-//        return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
+        if ($_SERVER['HTTP_VERSION_CODE'] >= 2.1) {
+            return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_wyx.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_kxs.png', 'type' => 'sdk', 'url' => 'op_novel'));
+        } else {
+            return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
+        }
     }
 }
