fcaee24ca71cb2d34b7ea05994f795e583037ccd
faaat <zjf580@163.com>
2021-03-24 09:45:02 +0800

    添加看小说玩游戏接口

--- app/core/Ad/StartMid.php
+++ app/core/Ad/StartMid.php
@@ -7,7 +7,7 @@ use Core\Ad;
 class StartMid extends Ad
 {
     protected function _getInfo() {
-        if ($_SERVER['HTTP_VERSION_CODE'] >= 2.1) {
+        if ($_SERVER['HTTP_VERSION_CODE'] >= 2.3) {
             return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_wyx.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_kxs.png', 'type' => 'sdk', 'url' => 'op_novel'));
         } else {
             return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
