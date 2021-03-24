80da9f33bdbeaeb684e83da68982305569a13b48
faaat <zjf580@163.com>
2021-03-24 09:32:47 +0800

    添加看小说玩游戏接口

--- app/core/Ad/StartMid.php
+++ app/core/Ad/StartMid.php
@@ -8,14 +8,7 @@ class StartMid extends Ad
 {
     protected function _getInfo() {
         if ($_SERVER['HTTP_VERSION_CODE'] >= 2.1) {
-            switch ($_SERVER['HTTP_SOURCE']) {
-                case 'huawei':
-                case 'tx':
-                    return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
-                    break;
-                default :
-                    return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_wyx.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_kxs.png', 'type' => 'sdk', 'url' => 'op_novel'));
-            }
+            return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_wyx.png', 'type' => 'sdk', 'url' => 'dl_game'), array('img' => OSS_HOST . 'ad/start_kxs.png', 'type' => 'sdk', 'url' => 'op_novel'));
         } else {
             return array(array('img' => OSS_HOST . 'ad/start_ydz.png', 'type' => 'interior', 'url' => 'sport'), array('img' => OSS_HOST . 'ad/start_yq.png', 'type' => 'interior', 'url' => 'invited'), array('img' => OSS_HOST . 'ad/start_mrfl.png', 'type' => 'interior', 'url' => 'task'));
         }
