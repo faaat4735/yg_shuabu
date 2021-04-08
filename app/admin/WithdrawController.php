b25cfef166545f483198e546ca003a83a3791a58
faaat <zjf580@163.com>
2021-04-08 10:59:37 +0800

    修复微信提现发放

--- app/admin/WithdrawController.php
+++ app/admin/WithdrawController.php
@@ -67,7 +67,7 @@ Class WithdrawController extends Controller {
                             break;
                         case 'wechat':
                             $wechatPay = new \Core\Wxpay();
-                            $returnStatus = $wechatPay->transfer($payInfo['withdraw_amount'], $payInfo['wechat_openid']);
+                            $returnStatus = $wechatPay->transfer($payInfo['withdraw_amount'], $payInfo['wechat_account']);
                             break;
                     }
                     if (TRUE === $returnStatus) {
